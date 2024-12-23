be62e11525badbfff4e319ec460674139b5e9761
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-12-22 21:32:24 -0300

    teste

--- src/main/java/repository/RamoAtividadesRepository.java
+++ src/main/java/repository/RamoAtividadesRepository.java
@@ -4,14 +4,16 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import com.lucas.model.RamoAtividade;
 
 public class RamoAtividadesRepository implements Serializable {
 	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	private EntityManager manager;
 	
@@ -25,7 +27,16 @@ public class RamoAtividadesRepository implements Serializable {
 	
 	
 	public List<RamoAtividade> pesquisa(String descricao) {
-		return null;
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		
+		//Equivalente ao JPQL
+		CriteriaQuery<RamoAtividade> criterialQuery = criteriaBuilder.createQuery(RamoAtividade.class);
+		Root<RamoAtividade> root = criterialQuery.from(RamoAtividade.class);
+		criterialQuery.select(root);
+		criterialQuery.where(criteriaBuilder.like(root.get("descricao"), descricao + "%"));
+		
+		TypedQuery<RamoAtividade> query = manager.createQuery(criterialQuery);
+		return query.getResultList();
 	}
 	
 	
